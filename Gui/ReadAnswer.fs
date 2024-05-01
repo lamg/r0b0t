669828d7171b053666d6424ec63d8e4d44dd175e
Luis Ángel Méndez Gort <lamg@protonmail.com>
2024-05-01 20:38:31 +0200

    mailbox working for dummy

--- Gui/ReadAnswer.fs
+++ Gui/ReadAnswer.fs
@@ -2,7 +2,7 @@ module ReadAnswer
 
 open System.Threading.Channels
 open Controls
-
+open Types
 
 type AddText = string option -> unit
 
@@ -28,6 +28,28 @@ let readAnswer (sa: StartAddText) (answer: Channel<string option>) =
   let rec loop () =
     task {
       let! r = answer.Reader.ReadAsync()
+
+      (fun _ ->
+        sa.addText r
+        false)
+      |> GLib.Idle.Add
+      |> ignore
+
+      if r.IsSome then
+        return! loop ()
+    }
+
+  sa.start ()
+  loop () |> Async.AwaitTask |> Async.Start
+
+type ReadStop =
+  { stop: unit -> Async<string option>
+    read: unit -> Async<string option> }
+
+let readAnswer2 (sa: StartAddText) (st: ReadStop) =
+  let rec loop () =
+    task {
+      let! r = st.read ()
 
       (fun _ ->
         sa.addText r
--- Lib/Providers/Dummy.fs
+++ Lib/Providers/Dummy.fs
@@ -16,3 +16,26 @@ let getProvider () =
   { name = "OpenAI"
     models = [ "Dummy" ]
     implementation = (fun _ (_, answer) -> provider answer) }
+
+let provider2 (inbox: MailboxProcessor<Message>) =
+  let rec loop (xs: string list) =
+    async {
+      // Receive a message
+      let! msg = inbox.Receive()
+      do! Async.Sleep 500
+
+      return!
+        match msg, xs with
+        | Question q, _ -> loop xs
+        | AnswerSegment chan, y :: ys ->
+          chan.Reply $" {y}"
+          loop ys
+        | Stop chan, _
+        | AnswerSegment chan, _ ->
+          chan.Reply "\n\n"
+          async { () }
+    }
+
+  let xs = Guid.NewGuid().ToString().Split("-")
+  let ys = "dummy" :: (List.ofArray xs)
+  loop ys
