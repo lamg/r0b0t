d604468c93b5b5804524c297842fc6b43abfba7a
Luis Ángel Méndez Gort <lamg@protonmail.com>
2024-07-21 17:20:18 +0200

    the great refactoring

---
+++ r0b0t_lib/Core.fs
@@ -0,0 +1,86 @@
+module r0b0tLib.Core
+
+open FSharp.Control
+
+type Key = Key of string
+type Model = Model of string
+type Prompt = Prompt of string
+type Provider = Provider of string
+
+type PngData =
+  { image: byte array
+    prompt: string
+    revisedPrompt: string }
+
+type LlmData =
+  | Word of string
+  | PngData of PngData
+
+type KeyProvider =
+  abstract member key: unit -> Key
+
+type DataConsumer =
+  abstract member consume: LlmData -> unit
+
+type Request =
+  | SetModel of Model
+  | StreamCompletion of Prompt
+  | SetProvider of Provider
+  | AvailableModels
+  | AvailableProviders
+
+type RequestProvider =
+  abstract member event: Event<Request>
+
+type Configuration = { model: Model; provider: Provider }
+
+type ConfigurationManager =
+  abstract member storeConfiguration: Configuration -> unit
+  abstract member loadConfiguration: unit -> Configuration
+
+type CompletionStreamer =
+  abstract member streamCompletion: Key -> Model -> Prompt -> AsyncSeq<LlmData>
+
+type AvailableModelsProvider =
+  abstract member availableModels: unit -> Model list
+
+type AvailableModelsConsumer =
+  abstract member consumeAvailableModels: Model list -> unit
+
+type AvailableProvidersProvider =
+  abstract member availableProviders : unit -> Provider list
+  
+type AvailableProviderConsumer =
+  abstract member consumeAvailableProviders: Provider list -> unit
+
+type StreamEnv =
+  inherit RequestProvider
+  inherit KeyProvider
+  inherit DataConsumer
+  inherit ConfigurationManager
+  inherit CompletionStreamer
+  inherit AvailableModelsProvider
+  inherit AvailableModelsConsumer
+  inherit AvailableProvidersProvider
+  inherit AvailableProviderConsumer
+
+let requestProcessor (env: StreamEnv) (conf: Configuration) (r: Request) =
+  let mutable c = conf
+
+  match r with
+  | SetModel m ->
+    c <- { c with model = m }
+    env.storeConfiguration c
+  | SetProvider p ->
+    c <- { c with provider = p }
+    env.storeConfiguration c
+  | StreamCompletion prompt ->
+    let key = env.key ()
+    let stream = env.streamCompletion key c.model prompt
+    stream |> AsyncSeq.iter env.consume |> Async.Start
+  | AvailableModels -> env.availableModels () |> env.consumeAvailableModels
+  | AvailableProviders -> env.availableProviders () |> env.consumeAvailableProviders
+
+let plugLogicToEnv (env: StreamEnv) =
+  let conf = env.loadConfiguration ()
+  env.event.Publish.Add(requestProcessor env conf)
---
+++ r0b0t_lib/OpenAI.fs
@@ -0,0 +1,44 @@
+﻿module r0b0tLib.OpenAI
+
+open FSharp.Control
+open OpenAI.Chat
+open OpenAI.Images
+
+open Core
+
+[<Literal>]
+let dalle3 = "dall-e-3"
+
+let imagine (Key key) (Prompt p) =
+  let client = ImageClient(dalle3, key)
+
+  let opts =
+    ImageGenerationOptions(
+      Quality = GeneratedImageQuality.High,
+      Size = GeneratedImageSize.W1792xH1024,
+      Style = GeneratedImageStyle.Vivid,
+      ResponseFormat = GeneratedImageFormat.Bytes
+    )
+
+  let png = client.GenerateImage(p, opts)
+
+  [ PngData
+      { image = png.Value.ImageBytes.ToArray()
+        prompt = p
+        revisedPrompt = png.Value.RevisedPrompt } ]
+  |> AsyncSeq.ofSeq
+
+let complete (Key key) (Model m) (Prompt p) =
+  let client = ChatClient(m, key, null)
+  let r = client.CompleteChatStreamingAsync p
+
+  r
+  |> AsyncSeq.ofAsyncEnum
+  |> AsyncSeq.map (fun x -> x.ContentUpdate |> Seq.head |> _.Text |> Word)
+
+let newOpenAIStreamer =
+  { new CompletionStreamer with
+      member _.streamCompletion k (Model m) p =
+        match m with
+        | _ when m = dalle3 -> imagine k p
+        | _ -> complete k (Model m) p }
